50e0fa1dc230d6d3749f38342218e71e04321b51
FraMuCoder <35667016+FraMuCoder@users.noreply.github.com>
2018-08-09 09:58:13 -0700

    add example also needed for test code

---
+++ example/menu.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# use this stop fbi if it is used for splash screen
+#sudo killall fbi
+
+# use this if you want to restart menu after any selection but abort ([ESC] or [Q])
+#while [ 1 ]
+#   do
+
+   ./frabenu -s2 3x2 menu_%x_%y.png
+
+   selection=$?
+
+   case $selection in
+   0) clear;
+      echo "You have select $selection => abort";
+      exit;
+      ;;
+   1) clear;
+      echo "You have select $selection => menu_1_1.png";
+      ;;
+   2) clear;
+      echo "You have select $selection => menu_2_1.png";
+      ;;
+   3) clear;
+      echo "You have select $selection => menu_3_1.png";
+      ;;
+   4) clear;
+      echo "You have select $selection => menu_1_2.png";
+      ;;
+   5) clear;
+      echo "You have select $selection => menu_2_2.png";
+      ;;
+   6) clear;
+      echo "You have select $selection => menu_3_2.png";
+      ;;
+   255) clear;
+      echo "You have select $selection => error";
+      exit
+      ;;
+   *) clear;
+      echo "You have select $selection => this should not happen";
+      exit
+      ;;
+   esac
+
+#done
